36a79c41339d07e4cc6cb552e3e88816c47441e5
firmboy <yangjian5268@gmail.com>
2020-09-26 22:58:36 +0800

    使用groovy解决springboot动态加载jar包失败的问题

--- src/main/java/com/yonyougov/http/util/RegisUtil.java
+++ src/main/java/com/yonyougov/http/util/RegisUtil.java
@@ -1,9 +1,8 @@
 package com.yonyougov.http.util;
 
-import com.itranswarp.compiler.JavaStringCompiler;
 import com.yonyougov.http.entity.InterfaceNode;
-import com.yonyougov.http.registered.RegisService;
 import com.yonyougov.http.repo.InterfaceRepo;
+import groovy.lang.GroovyClassLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -50,41 +49,26 @@ public class RegisUtil {
 
     public static void regis(String serviceName, String addr, String result) {
         try {
-            JavaStringCompiler compiler = new JavaStringCompiler();
-            Map<String, byte[]> results = null;
             String replace = StringUtils.replace(JAVA_SOURCE_CODE, "SERVICE_NAME", serviceName);
             replace = StringUtils.replace(replace, "ADDR", addr);
             result = result.replace("\"", "\\\"");
-
             //去除回车换行
             Matcher m = p.matcher(result);
             result = m.replaceAll("");
-
-            ClassLoader classLoader1 = RegisUtil.class.getClassLoader();
-            log.info("classLoader1类加载器：" + classLoader1.toString());
-            ApplicationContext applicationContext = SpringBeanUtils.getApplicationContext();
-            RegisService bean = applicationContext.getBean(RegisService.class);
-            ClassLoader classLoader = bean.getClass().getClassLoader();
-            log.info("bean类加载器：" + classLoader.toString());
-
+            
             replace = StringUtils.replace(replace, "RESULT", result);
-            //log.info(replace);
+            GroovyClassLoader groovyClassLoader = new GroovyClassLoader();
+            Class aClass = groovyClassLoader.parseClass(replace);
 
-            Class<?> clazz = null;
-
-//            SpringBootMemoryClassLoader instrance = SpringBootMemoryClassLoader.getInstrance();
-//            instrance.registerJava(serviceName + "TemplateController.java", replace);
-//            clazz = instrance.findClass(serviceName + "TemplateController.java");
-
-            results = compiler.compile(serviceName + "TemplateController.java", replace);
-            byte[] buf = results.get(serviceName + "TemplateController.java");
-            clazz = compiler.loadClass("com.yonyougov.http.registered." + serviceName + "TemplateController", results);
-            //URLClassLoader classLoader = (URLClassLoader) RegisUtil.class.getClassLoader();
-            //classLoader.defineClass(serviceName + "TemplateController.java", buf, 0, buf.length);
-            MappingRegulator.controlCenter(clazz, applicationContext, 1);
+            ApplicationContext applicationContext = SpringBeanUtils.getApplicationContext();
+            MappingRegulator.controlCenter(aClass, applicationContext, 1);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static String buildClassPath(String s) {
+        return s;
     }
 
 
